b175e763cf8b45bcb28f88ca8fbc6fa9f0e12a3e
Tianhao WU <collinbeyond@gmail.com>
2018-11-22 22:06:54 +0100

    Update calculSimilarityAllLayers.py

--- calculSimilarityAllLayers.py
+++ calculSimilarityAllLayers.py
@@ -1,17 +1,14 @@
 import pandas as pd
-#import numpy as np
 import sys
-import ast
 
-output_file_name='similarityAllLayers'
+input1 = sys.argv[1]
+input2 = sys.argv[2]
+input3 = sys.argv[3]
+output_file_name=sys.argv[4]
 
-input_file_name='similarity_1'
-input_seq_1 = pd.read_csv(input_file_name,header=None, encoding = "UTF-8", sep='\t')
-input_file_name='similarity_01'
-input_seq_01 = pd.read_csv(input_file_name,header=None, encoding = "UTF-8", sep='\t')
-input_file_name='similarity_001'
-input_seq_001 = pd.read_csv(input_file_name,header=None, encoding = "UTF-8", sep='\t')
+df1 = pd.read_csv(input1,header=None, encoding = "UTF-8", sep='\t')
+df2 = pd.read_csv(input2,header=None, encoding = "UTF-8", sep='\t')
+df3 = pd.read_csv(input3,header=None, encoding = "UTF-8", sep='\t')
 
-
-dfsum=input_seq_1+2*input_seq_01+4*input_seq_001
+dfsum=df1+2*df2+4*df3
 dfsum.to_csv(output_file_name,header=None, index=None, sep='\t')

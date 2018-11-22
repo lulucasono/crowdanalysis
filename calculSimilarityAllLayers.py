85e1e77635c8cdf5a3adc802fa13c5e1004bfe5a
Tianhao WU <collinbeyond@gmail.com>
2018-11-22 22:54:23 +0100

    Update calculSimilarityAllLayers.py

--- calculSimilarityAllLayers.py
+++ calculSimilarityAllLayers.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import sys
 
-input1 = sys.argv[1]
-input2 = sys.argv[2]
-input3 = sys.argv[3]
-output_file_name=sys.argv[4]
+n = int(sys.argv[1])
+input_table = ['' for i in range(n)]
+for i in range(n):
+    input_table[i] = sys.argv[2+i]
+output_file_name=sys.argv[2+n]
 
-df1 = pd.read_csv(input1,header=None, encoding = "UTF-8", sep='\t')
-df2 = pd.read_csv(input2,header=None, encoding = "UTF-8", sep='\t')
-df3 = pd.read_csv(input3,header=None, encoding = "UTF-8", sep='\t')
+dfs = []
 
-dfsum=df1+2*df2+4*df3
+ssum = 0
+for i in range(n):
+    dfs.append(pd.read_csv(input_table[i],header=None, encoding = "UTF-8", sep='\t'))
+
+dfsum=dfs[i]
+for i in range(1,n):
+    dfsum += 2**i*dfs[i]
+    
 dfsum.to_csv(output_file_name,header=None, index=None, sep='\t')
